a03f561751c4744c02858fac9672b4d1b9e60845
thierry louppe <tlouppe@gmail.com>
2021-10-22 05:37:40 +0100

    init gestion article

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 
 //Ajout de sweetAlert2
 window.Swal = require("sweetalert2");
+window.Daterange = require("daterangepicker");
 
 import "admin-lte/plugins/jquery/jquery";
 import "admin-lte/plugins/bootstrap/js/bootstrap.bundle";
